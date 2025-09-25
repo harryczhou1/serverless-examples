4dd238b8ee440c5430220076011a8591574941a9
harryczhou1 <harryczhou1@gmail.com>
2025-09-24 20:12:54 -0400

    Update constants.py

--- deploy_runpod/src/constants.py
+++ deploy_runpod/src/constants.py
@@ -1,3 +1,3 @@
-DEFAULT_MODEL_DIR = "/model"   # local model cache
-DEFAULT_MODEL_NAME = "mistralai/Mistral-7B-v0.1"
-DEFAULT_DEVICE = "cuda"
+MODEL_DIR = "/model"   # local model cache
+MODEL_NAME = "mistralai/Mistral-7B-v0.1"
+DEVICE = "cuda"
