70df113eeacd02afb42d4ec6fb7d88e1e7cf5850
harryczhou1 <harryczhou1@gmail.com>
2025-10-01 22:04:50 -0400

    Update constants.py

--- deploy_runpod/src/constants.py
+++ deploy_runpod/src/constants.py
@@ -1,10 +1,11 @@
 import os
+from pathlib import Path
 
 # Default device to use if not overridden by environment
 DEFAULT_DEVICE = os.getenv("DEVICE", "cuda")
 
 # Default directory where the Hugging Face model will be downloaded / loaded
-DEFAULT_MODEL_DIR = os.getenv("MODEL_DIR", "/models/mistral")
+DEFAULT_MODEL_DIR = str(Path(os.getenv("MODEL_DIR", "/model")).resolve())
 
 # Default Hugging Face model ID (can be overridden in environment)
 DEFAULT_MODEL_NAME = os.getenv("MODEL_NAME", "mistralai/Mistral-7B-v0.1")
